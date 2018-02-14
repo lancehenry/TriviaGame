e06df1e580f7fa49670d51e6ead6d9cdd264b331
lschmittling <lanceschmittling@Lances-MBP>
2018-02-13 22:26:29 -0600

    Questions added, no code yet

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+MARVEL TRIVIA GAME
+
+What newspaper does Peter Parker work for?
+A. The Daily Planet *B. The Daily Bugle C. The New York Times D. The Rolling Stone
+
+The Fantastic Four have their headquarters in what building?
+A. Stark Tower B. Fantastic Headquarters *C. Baxter Building D. Xavier Institute
+
+What is Captain America's shield made out of?
+*A. Vibranium B. Adamantium C. Tin D. Aluminum
+
+What is the Winter Soldier's real name?
+A. Ron Burgundy B. Michael Scott C. Steve Rogers *D. Bucky Barnes
+
+What is Wolverine's real name?
+A. Billy *B. Logan C. Creed D. James
+
+Who came up with the team name 'Avengers'?
+A. Captain America B. Ironman *C. Wasp D. Hulk
+
+Who is the highest ranking SHIELD agent?
+A. Bill B. Ted *C. Nick Fury D. Black Widow
+
+What is the name of Thor's hammer?
+*A. Mjolnir B. Thanos C. Namor D. Joshua
+
+Black Panther lives in what fictional country?
+A. Galactus *B. Wakanda C. Westworld D. Enterprise
+
+Before becoming the Sorcerer Supreme, Doctor Strange was what?
+A. Pilot B. Scuba Diver C. Yoga Instructor *D. Neurosurgeon
+
+How did the Hulk get his powers?
+A. Space B. The Sun *C. Gamma Rays D. Gravity
+
+*/
